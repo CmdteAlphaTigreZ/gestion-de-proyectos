e6cce159e64fffe9c87e4ffa0b5a486bdaf25ab3
CATZ <13862383+CmdteAlphaTigreZ@users.noreply.github.com>
2024-07-01 02:54:04 -0400

    Limpieza de código y corrección ortográfica en funciones de formato

--- proyectos.py
+++ proyectos.py
@@ -26,30 +26,12 @@ class Proyecto:
         self.gerente = gerente
         self.equipo = equipo
         self.__tareas = Lista()
-        self.__pila_prioridades = Pila()
-        self.__cola_vencimientos = Cola()
 
     @property
     def tareas(self): return self.__tareas
 
-    @property
-    def pila_prioridades(self): return self.__pila_prioridades
-
-    @property
-    def cola_vencimientos(self): return self.__cola_vencimientos
-
     def agregar_tarea(self, tarea):
         self.tareas.append(tarea)
-
-    def agregar_pila(self, pila_prioridades):
-        self.pila_prioridades.append(pila_prioridades)
-
-    def agregar_prioridad(self, tarea_prioritaria):
-        self.pila_prioridades.apilar(tarea_prioritaria)
-        print(f"Tarea prioritaria '{tarea_prioritaria.nombre}' agregada.")
-
-    def agregar_cola(self, cola_vencimientos):
-        self.cola_vencimientos.append(cola_vencimientos)
 
     def __format__(self, formato):
         if formato == "":
@@ -58,7 +40,7 @@ class Proyecto:
             raise ValueError("la especificación de formato debe ser '' o 'g'")
         resultado = ['ID: {self.id}',
                      'Nombre: "{self.nombre}"',
-                     'Descripcion:\n{descripcion}',
+                     'Descripción:\n{descripcion}',
                      'Fecha de inicio: {fecha_inicio}',
                      'Fecha de vencimiento: {fecha_vencimiento}',
                      'Estado actual: "{self.estado}"',
@@ -90,22 +72,25 @@ class Tarea:
         self,
         id_,
         nombre,
-        empresa_cliente,
         descripcion,
         fecha_inicio,
         fecha_vencimiento,
         estado,
         porcentaje,
+        empresa_cliente,
     ):
         self.id = id_
         self.nombre = nombre
-        self.empresa_cliente = empresa_cliente
         self.descripcion = descripcion
         self.fecha_inicio = fecha_inicio
         self.fecha_vencimiento = fecha_vencimiento
         self.estado = estado
         self.porcentaje = porcentaje
-        self.subtareas = Lista()
+        self.empresa_cliente = empresa_cliente
+        self.__subtareas = Lista()
+
+    @property
+    def subtareas(self): return self.__subtareas
 
     def __format__(self, formato):
         if formato == "":
@@ -114,7 +99,7 @@ class Tarea:
             raise ValueError("la especificación de formato debe ser '' o 'g'")
         resultado = ['ID: {self.id}',
                      'Nombre: "{self.nombre}"',
-                     'Descripcion:\n{descripcion}',
+                     'Descripción:\n{descripcion}',
                      'Fecha de inicio: {fecha_inicio}',
                      'Fecha de vencimiento: {fecha_vencimiento}',
                      'Estado actual: "{self.estado}"',
@@ -139,9 +124,3 @@ class Tarea:
         return resultado.format(self=self)
 
 
-class Subtarea:
-    def __init__(self, id_, nombre, descripcion, estado):
-        self.id = id_
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.estado = estado
