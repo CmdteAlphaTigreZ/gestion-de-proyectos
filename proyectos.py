ae5b1f382ebba339c1f74fb6a9d09a55e68f73ea
CATZ <13862383+CmdteAlphaTigreZ@users.noreply.github.com>
2024-07-15 20:18:06 -0400

    Inicio de implementación de Gestor en proyectos.py
    Adición de funciones para cambios de contexto de gestión y adición de
    lista de empresas gestionadas.

--- proyectos.py
+++ proyectos.py
@@ -183,14 +183,123 @@ class Tarea:
 class Gestor:
     "Gestor de proyectos multiempresa."
 
+    ERROR_NO_FORZADO = 1
+    ERROR_NO_EMPRESA = 10
+    __MSG_ERROR_NO_EMPRESA = "No hay empresa con proyectos siendo gestionados"
+    ERROR_PROYECTO_NO_PERTENECE = 11
+    ERROR_NO_PROYECTO = 20
+    ERROR_TAREA_NO_PERTENECE = 21
+    ERROR_NO_TAREA = 30
+
     def __init__(self):
+        self.id_empresa_max = 0
         self.id_proyecto_max = 0
         self.id_tarea_max = 0
+        self.__empresas = ListaEnlazada()
         self.empresa = None
         self.proyecto = None
         self.__tareas = Pila()
 
     @property
+    def empresas(self):
+        "Lista que almacena todas las empresas gestionadas."
+        return self.__empresas
+
+    @property
     def tareas(self):
         "Pila que almacena la cadena de subtareas en edición."
         return self.__tareas
+
+    def gestionar_proyectos(self, empresa, forzar=False):
+        if not forzar \
+            and not (self.empresa is None and self.proyecto is None
+                     and len(self.__tareas) == 0):
+            raise RuntimeError("Debe salir de la gestión de la empresa actual"
+                               " para cambiar la empresa gestionada",
+                               Gestor.ERROR_NO_FORZADO)
+        self.__tareas.vaciar()
+        self.proyecto = None
+        self.empresa = empresa
+
+    def gestionar_tareas(self, proyecto, forzar=False):
+        if not forzar \
+            and not (self.proyecto is None and len(self.__tareas) == 0):
+            raise RuntimeError("Debe salir de la gestión del proyecto actual"
+                               " para cambiar el proyecto gestionado",
+                               Gestor.ERROR_NO_FORZADO)
+        if self.empresa is None:
+            raise RuntimeError(Gestor.__MSG_ERROR_NO_EMPRESA,
+                               Gestor.ERROR_NO_EMPRESA)
+        if self.empresa.proyectos.buscar(ClaveValor(proyecto.id, None)) is None:
+            raise RuntimeError("El proyecto no pertenece a la empresa en edición",
+                               Gestor.ERROR_PROYECTO_NO_PERTENECE)
+        self.__tareas.vaciar()
+        self.proyecto = proyecto
+
+    def gestionar_subtareas(self, tarea):
+        if self.empresa is None:
+            raise RuntimeError(Gestor.__MSG_ERROR_NO_EMPRESA,
+                               Gestor.ERROR_NO_EMPRESA)
+        if self.proyecto is None:
+            raise RuntimeError("No hay proyecto con tareas siendo gestionadas",
+                               Gestor.ERROR_NO_PROYECTO)
+        if len(self.__tareas) == 0:
+            raiz = self.proyecto.tareas.raiz
+        else:
+            raiz = self.__tareas.cima.subtareas.raiz
+        if util.buscar(
+            lambda nodo: nodo.valor is not None and nodo.valor.id == tarea.id,
+            raiz.hijos()
+        ) is None:
+            raise RuntimeError("La tarea no pertenece al proyecto o tarea padre"
+                               " en edición", Gestor.ERROR_TAREA_NO_PERTENECE)
+        self.__tareas.insertar(tarea)
+
+    def regresar_a_empresas(self, forzar=False):
+        if not forzar \
+            and not (self.proyecto is None and len(self.__tareas) == 0):
+            raise RuntimeError("Debe regresar de la edición de tareas antes de"
+                               " regresar de la edición de proyectos",
+                               Gestor.ERROR_NO_FORZADO)
+        if self.empresa is None:
+            raise RuntimeError("No se está gestionando los proyectos de una"
+                               " empresa.  El contexto actual ya es la gestión"
+                               " de empresas", Gestor.ERROR_NO_EMPRESA)
+        # POR HACER: Guardar proyectos de la empresa
+        self.__tareas.vaciar()
+        self.proyecto = None
+        self.empresa = None
+
+    def regresar_a_proyectos(self, forzar=False):
+        if not forzar and len(self.__tareas) != 0:
+            raise RuntimeError("Debe regresar de la edición de subtareas antes"
+                               " de regresar de la edición de tareas del"
+                               " proyecto", Gestor.ERROR_NO_FORZADO)
+        if self.proyecto is None:
+            raise RuntimeError("No se está gestionando las tareas de un"
+                               " proyecto.  El contexto actual ya es la gestión"
+                               " de proyectos", Gestor.ERROR_NO_PROYECTO)
+        # POR HACER: Guardar tareas del proyecto
+        self.__tareas.vaciar()
+        self.proyecto = None
+
+    def regresar_a_tarea_padre(self):
+        if len(self.__tareas) != 0:
+            raise RuntimeError("No se está gestionando las subtareas de una"
+                               " tarea.  El contexto actual es la gestión"
+                               " de las tareas principales de un proyecto",
+                               Gestor.ERROR_NO_TAREA)
+        # POR HACER: Guardar subtareas de la tarea
+        self.__tareas.extraer()
+
+    def regresar(self):
+        if len(self.__tareas) != 0:
+            self.regresar_a_tarea_padre()
+        elif self.proyecto is not None:
+            self.regresar_a_proyectos()
+        elif self.empresa is not None:
+            self.regresar_a_empresas()
+        else:
+            raise RuntimeError("El contexto actual ya es la gestión de empresas"
+                               ".  No hay dónde regresar",
+                               Gestor.ERROR_NO_EMPRESA)
